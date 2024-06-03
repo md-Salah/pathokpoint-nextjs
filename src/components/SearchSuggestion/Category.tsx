c6d91b2d57d2f0652fc2f46a63a58a18d4b83f42
мd ѕαℓαнuddιn <salahuddin3652@gmail.com>
2024-06-03 19:43:34 +0600

    bug fix

--- src/components/SearchSuggestion/Category.tsx
+++ src/components/SearchSuggestion/Category.tsx
@@ -14,9 +14,9 @@ const Category = ({ category }: Props) => {
         <div className="flex items-center gap-3 mt-0 border-b border-base-300 bg-base-200 group p-1 pl-2">
           <div className="avatar">
             <div className="mask mask-squircle w-12 h-12 text-2xl text-center group-hover:opacity-80">
-              {category.src ? (
+              {category.image?.src ? (
                 <Image
-                  src={category.src}
+                  src={category.image.src}
                   alt={category.name[0]}
                   width={48}
                   height={48}
