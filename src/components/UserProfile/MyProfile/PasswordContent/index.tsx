6d9dcdbb5d68acffbf8563f8aaca65a1ed9a6e07
Harun-techie53 <u1809030@student.cuet.ac.bd>
2024-07-10 06:23:31 -0400

    5 digit input form modal added to the my profile password tab content

--- src/components/UserProfile/MyProfile/PasswordContent/index.tsx
+++ src/components/UserProfile/MyProfile/PasswordContent/index.tsx
@@ -1,44 +1,82 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
+import CodeInputForm from "./CodeInputFormModal";
 
 const PasswordContent = () => {
+  const inputs = Array.from({ length: 5 }, (_, i) => i);
+  const handleOpenInputDigitsModal = () => {
+    const modalElement = document.getElementById(
+      "input_digits_modal"
+    ) as HTMLDialogElement;
+    modalElement.showModal();
+  };
+
+  const handleCloseInputDigitsModal = () => {
+    const modalElement = document.getElementById(
+      "input_digits_modal"
+    ) as HTMLDialogElement;
+    modalElement.close();
+  };
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+    const input = e.target;
+    if (input.value.length === 1 && index < inputs.length - 1) {
+      (
+        document.getElementById(`input-${index + 1}`) as HTMLInputElement
+      ).focus();
+    }
+  };
   return (
-    <div className="bg-white rounded-lg w-full h-screen py-14">
-      <div className="space-y-4 w-1/2 mx-auto">
-        <div className="flex flex-col space-y-2">
-          <label className="text-sm font-semibold text-black03">
-            Current Password
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Current Password"
-            className="input input-bordered text-base text-black02 w-full"
-          />
+    <>
+      <div className="bg-white rounded-lg w-full h-screen py-14">
+        <div className="space-y-4 w-1/2 mx-auto">
+          <div className="flex flex-col space-y-2">
+            <label className="text-sm font-semibold text-black03">
+              Current Password
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Current Password"
+              className="input input-bordered text-base text-black02 w-full"
+            />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label className="text-sm font-semibold text-black03">
+              New Password
+            </label>
+            <input
+              type="text"
+              placeholder="Enter New Password"
+              className="input input-bordered text-base text-black02"
+            />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label className="text-sm font-semibold text-black03">
+              Confirm Password
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Confirm Password"
+              className="input input-bordered text-base text-black02"
+            />
+          </div>
         </div>
-        <div className="flex flex-col space-y-2">
-          <label className="text-sm font-semibold text-black03">
-            New Password
-          </label>
-          <input
-            type="text"
-            placeholder="Enter New Password"
-            className="input input-bordered text-base text-black02"
-          />
-        </div>
-        <div className="flex flex-col space-y-2">
-          <label className="text-sm font-semibold text-black03">
-            Confirm Password
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Confirm Password"
-            className="input input-bordered text-base text-black02"
-          />
+        <div className="w-1/2 flex justify-end pt-14 mx-auto">
+          <button
+            className="btn btn-primary px-10 text-base"
+            onClick={handleOpenInputDigitsModal}
+          >
+            Update
+          </button>
         </div>
       </div>
-      <div className="w-1/2 flex justify-end pt-14 mx-auto">
-        <button className="btn btn-primary px-10 text-base">Update</button>
-      </div>
-    </div>
+      <dialog id="input_digits_modal" className="modal">
+        <CodeInputForm
+          handleInput={handleInput}
+          inputs={inputs}
+          onClose={handleCloseInputDigitsModal}
+        />
+      </dialog>
+    </>
   );
 };
 
