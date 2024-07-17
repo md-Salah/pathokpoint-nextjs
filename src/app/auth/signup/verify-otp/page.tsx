9344f48539ce3fc110c54a8b52c888be44c637fc
мd ѕαℓαнuddιn <salahuddin3652@gmail.com>
2024-07-17 12:53:26 +0600

    bug fix

--- src/app/auth/signup/verify-otp/page.tsx
+++ src/app/auth/signup/verify-otp/page.tsx
@@ -1,11 +1,15 @@
 import { VerifyOTP } from "@/components";
 
 const SignUp = () => {
+  const handleOTPSubmit = () => {
+    //verify otp
+  };
+
   return (
     <div className="layout-container layout-mt layout-p bg-white">
       <div className="my-5 grid">
         <div className="mx-auto max-w-xs">
-          <VerifyOTP />
+          <VerifyOTP handleOTPSubmit={handleOTPSubmit} />
         </div>
       </div>
     </div>
