03b49701960ae4ea30749c30bdee1b80314ab60d
мd ѕαℓαнuddιn <salahuddin3652@gmail.com>
2024-09-04 20:11:07 +0600

    opengraph

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -2,30 +2,22 @@ import { ImageResponse } from 'next/og';
 
 import { defaultSrc } from '@/constants';
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 // Image metadata
-export const alt = 'Pathok Point';
+export const alt = "Pathok Point";
 export const size = {
   width: 1200,
   height: 630,
 };
 
-
-export const contentType = 'image/png';
+export const contentType = "image/png";
 
 // Image generation
 export default async function Image() {
-    // console.log('lal')
-  // Load the font
-//   const interSemiBold = fetch(
-//     new URL('./Inter-SemiBold.ttf', import.meta.url)
-//   ).then((res) => res.arrayBuffer());
-
-//   // Load the logo
-//   const logo = fetch(
-//     new URL(defaultSrc.logo, import.meta.url) // Replace with the correct path to your logo file
-//   ).then((res) => res.arrayBuffer());
+  const logoSrc = await fetch(new URL("/logo/logo.png", import.meta.url)).then(
+    (res) => res.arrayBuffer()
+  );
 
   return new ImageResponse(
     (
@@ -33,36 +25,36 @@ export default async function Image() {
       <div
         style={{
           fontSize: 64,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          position: 'relative',
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          position: "relative",
         }}
       >
-        {/* Logo */}
         <img
-          src={"/logo/logo.png"}
+          // @ts-ignore
+          src={logoSrc}
           alt="Pathok Point Logo"
-          style={{ width: 300, height: 'auto', marginBottom: 20 }}
+          style={{ width: 300, height: "auto", marginBottom: 20 }}
         />
-        {/* Text */}
+        <h1>Kire mama</h1>
         <div>Pathok Point</div>
       </div>
     ),
     {
       ...size,
-    //   fonts: [
-    //     {
-    //       name: 'Inter',
-    //       data: await interSemiBold,
-    //       style: 'normal',
-    //       weight: 600, // Set weight to match "SemiBold"
-    //     },
-    //   ],
+      //   fonts: [
+      //     {
+      //       name: 'Inter',
+      //       data: await interSemiBold,
+      //       style: 'normal',
+      //       weight: 600, // Set weight to match "SemiBold"
+      //     },
+      //   ],
     }
   );
 }
