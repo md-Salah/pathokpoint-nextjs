544113a749ff714a7e667e13b3df6346dac5d123
мd ѕαℓαнuddιn <salahuddin3652@gmail.com>
2024-09-13 02:50:04 +0600

    prevent useUser always fetch from db

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,16 +1,17 @@
 "use client";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import useSWR from 'swr';
 
 import { useToken } from '@/hooks';
 import { User } from '@/interface';
 import { updateUser } from '@/redux/features/auth-slice';
-import { AppDispatch } from '@/redux/store';
+import { AppDispatch, RootState } from '@/redux/store';
 import axiosInstance from '@/utils/axiosConfig';
 
 const useUser = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { token } = useToken();
+  const { user } = useSelector((state: RootState) => state.auth);
 
   const fetcher = async (url: string, token: string | null | undefined) => {
     try {
@@ -32,8 +33,16 @@ const useUser = () => {
 
   const { data, isLoading }: { data: User | null; isLoading: boolean } = useSWR(
     ["/user/me", token],
-    ([url, token]) => fetcher(url, token)
+    ([url, token]) => fetcher(url, token),
+    { revalidateOnFocus: false }
   );
+
+  if (user) {
+    return {
+      user,
+      isLoading: false,
+    };
+  }
 
   return {
     user: data,
