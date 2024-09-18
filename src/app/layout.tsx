47e1059f7b57021e6d8fd75a0988e933dea1fe63
мd ѕαℓαнuddιn <salahuddin3652@gmail.com>
2024-09-18 17:27:43 +0600

    facebook pixel added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import './globals.css';
 
 import { Anek_Bangla, Inter } from 'next/font/google';
+import Script from 'next/script';
 
 import { Footer, Navbar, TopBanner } from '@/components';
 import { ReduxProvider } from '@/redux/provider';
@@ -49,6 +50,39 @@ export default function RootLayout({
             {children}
             <Footer />
           </main>
+
+          {process.env.NEXT_PUBLIC_META_PIXEL_ID && (
+            <>
+              <Script>
+                {`
+            <!-- Meta Pixel Code -->
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
+
+            var add = window.location.toString();
+            if (add.indexOf("/#!/") != -1){
+              fbq('track', 'PageView');
+            }
+            <!-- End Meta Pixel Code -->
+          `}
+              </Script>
+              <noscript>
+                <img
+                  height="1"
+                  width="1"
+                  style={{ display: "none" }}
+                  src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_META_PIXEL_ID}&ev=PageView&noscript=1`}
+                />
+              </noscript>
+            </>
+          )}
         </body>
         {process.env.NEXT_PUBLIC_MEASUREMENT_ID && (
           <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_MEASUREMENT_ID} />
